4306cc051ca72720b398a06d287d0ad7ebf2196f
Kuuhakuu <kuuhakuu@Kuuhakuus-MacBook-Pro.local>
2023-11-27 07:58:34 +0700

    fourth commit

--- Tuto/app/src/main/java/network/Client.java
+++ Tuto/app/src/main/java/network/Client.java
@@ -1,19 +1,24 @@
 package network;
 
-
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.security.Policy;
 
+import model.Board;
 import model.Cell;
 import model.Pair;
 import model.Player;
+import model.User;
 
 public class Client {
-    public static Socket socket;
-    private static String serverAddress = "127.0.0.1"; // serverAddress
+    private static Socket socket;
+    private static String serverAddress = "26.83.233.30";
     private static int port = 4444;
     private static DataInputStream dataInputStream;
     private static ObjectOutputStream objectOutputStream;
@@ -23,26 +28,30 @@ public class Client {
 
     }
 
-    public Player findMatch(Player player) {
-        return sendObjectToServer(player, "find-match");
-    }
 
-    public Player sendObjectToServer(Object object, String requestType) {
+    public Player findMatch(Player player) {
         try {
             socket = new Socket(serverAddress, port);
+            sendRequestType("find-match");
 
             objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            Pair<String, Object> requestObject = new Pair<String, Object>(requestType, object);
-            objectOutputStream.writeObject(requestObject);
+            objectOutputStream.writeObject(player);
             objectOutputStream.flush();
 
             objectInputStream = new ObjectInputStream(socket.getInputStream());
             Player response = null;
             long timeoutMillis = System.currentTimeMillis() + 10000;
             while(System.currentTimeMillis() < timeoutMillis) {
-                response = (Player) objectInputStream.readObject();
-                if(response != null) break;
-
+                if(dataInputStream.available() > 0) {
+                    response = (Player)objectInputStream.readObject();
+                    if(response != null) break;
+                } else {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
             if (response != null) return response;
         } catch (Exception e) {
@@ -51,21 +60,17 @@ public class Client {
         return null;
     }
 
-    public Pair<String, Player> receiveObjectInstruction() {
+    public static void sendRequestType(String requestType) {
         try {
-            objectInputStream = new ObjectInputStream(socket.getInputStream());
-            Pair<String, Player> instruction = (Pair<String, Player>)objectInputStream.readObject();
-
-            return instruction;
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF(requestType);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
     }
 
     public static String receiveInstruction() {
         try {
-            dataInputStream = new DataInputStream(socket.getInputStream());
             String instruction = dataInputStream.readUTF();
             return instruction;
         } catch (Exception e) {
@@ -74,16 +79,18 @@ public class Client {
         return "";
     }
 
+
     public static void sendCoordinate(Cell cell) {
         try {
-            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            objectOutputStream.writeObject(cell);
-            objectOutputStream.flush();
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF(cell.toString());
+            dataOutputStream.flush();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static Cell receiveCoordinate(){
+
+    public static Cell receiveCoordinate() {
         try {
             objectInputStream = new ObjectInputStream(socket.getInputStream());
             Cell cell = (Cell) objectInputStream.readObject();
@@ -93,7 +100,6 @@ public class Client {
         }
         return null;
     }
-
     public Socket getSocket() {
         return socket;
     }
@@ -101,5 +107,6 @@ public class Client {
     public void setSocket(Socket socket) {
         this.socket = socket;
     }
+
 
 }
